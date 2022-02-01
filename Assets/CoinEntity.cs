9320a9cb768ad038b82815f7634429dfc28227f8
Ishimine <felipecalafate@hotmail.com>
2022-02-01 09:42:57 -0300

    Info Panel + Coin Hover

--- Assets/CoinEntity.cs
+++ Assets/CoinEntity.cs
@@ -6,25 +6,47 @@ using UnityEngine.UIElements;
 
 public class CoinEntity : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    [ShowInInspector]  private event Action _coinDroppedCallback;
+    [ShowInInspector] private event Action CoinDroppedCallback;
+    [ShowInInspector] public event Action OnDragBegin;
+    
+    
+    [ShowInInspector] public event Action<bool> OnHover;
 
-    public Coin coin;
+    [SerializeField]private Coin coin;
+    public Coin Coin => coin;
+ 
+    [SerializeField]private MeshRenderer meshRenderer;
+
+    private Material _material;
+    private Material Mat=> _material ??= meshRenderer.material;
     
     [ShowInInspector] public bool IsClicked { get; private set; }  = false;
 
-    [ShowInInspector]    private bool _isHover = false;
+    [ShowInInspector] private bool _isHover = false;
+    private static readonly int BaseText = Shader.PropertyToID("BaseTex");
+    private static readonly int IconHeadTex = Shader.PropertyToID("IconHeadTex");
+    private static readonly int IconTailTex = Shader.PropertyToID("IconTailTex");
+
     public bool IsHover
     {
         get => _isHover && !IsClicked;
         private set => _isHover = value;
     }
-
+    
     public void Initialize(Coin nCoin, Action coinDroppedCallback)
     {
         coin = nCoin;
-        _coinDroppedCallback = coinDroppedCallback;
+        CoinDroppedCallback = coinDroppedCallback;
+        
+        SetBaseTexture(coin.tier);
+        SetHeadTexture(coin.HeadCoinIcon);
+        SetTailTexture(coin.TailCoinIcon);
     }
 
+    [Button] private void SetBaseTexture(int index) => Mat.SetTexture(BaseText, CoinManager.GetCoinBaseTextureFromTier(index));
+    [Button] private void SetHeadTexture(Texture texture) => Mat.SetTexture(IconHeadTex, texture);
+    [Button] private void SetTailTexture(Texture texture) => Mat.SetTexture(IconTailTex, texture);
+    
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Click");
@@ -32,6 +54,7 @@ public class CoinEntity : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        OnDragBegin?.Invoke();
         Debug.Log("PointerDOwn");
         IsClicked = true;
     }
@@ -43,8 +66,7 @@ public class CoinEntity : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
         if(!IsClicked)
             return;
         IsClicked = false;
-        
-        _coinDroppedCallback.Invoke();
+        CoinDroppedCallback.Invoke();
     }
 
     private void FixedUpdate()
@@ -61,13 +83,18 @@ public class CoinEntity : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
     {
         Debug.Log("OnPointerEnter");
         IsHover = true;
+        OnHover?.Invoke(true);
+        InfoPanelUI.SetInfo(coin);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("OnPointerExit");
         IsHover = false;
+        OnHover?.Invoke(false);
+        InfoPanelUI.Clear();
     }
+
 }
 
 
--- Assets/Scripts/MainGame/Coin.cs
+++ Assets/Scripts/MainGame/Coin.cs
@@ -7,5 +7,13 @@ public class Coin
     
     [SerializeReference] public BattleEffect headEffect;
     [SerializeReference] public BattleEffect tailEffect;
-    
+
+    public int ValueFromTier => CoinManager.GetValueForTier(tier);
+    public Sprite HeadIcon => headEffect.Icon;
+    public Sprite TailIcon => tailEffect.Icon;
+    public Texture HeadCoinIcon => headEffect.CoinTexture;
+    public Texture TailCoinIcon => tailEffect.CoinTexture;
+
+    public string HeadDescription() => headEffect.Description(this);
+    public string TailDescription() => tailEffect.Description(this);
 }

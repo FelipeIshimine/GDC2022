70eecde06d94afb2492b225d01b6ff5d2aab9ce1
Ishimine <felipecalafate@hotmail.com>
2022-02-05 20:29:14 -0300

    TurnState

---
+++ Assets/Scripts/StateMachine/TurnState.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class TurnState : AsyncState
+{
+    public enum TurnType { Player, Enemy }
+    private readonly TurnType _turnType;
+    private readonly BattleUnit[] _units;
+    private readonly DeckBattleData _deckBattleData;
+    private readonly Action _onDoneCallback;
+    private readonly Action _onQuitCallback;
+        
+    public TurnState(BattleUnit[] units, TurnType turnType, DeckBattleData deckBattleData,Action onQuitCallback, Action onDoneCallback)
+    {
+        _units = units;
+        _turnType = turnType;
+        _deckBattleData = deckBattleData;
+        _onQuitCallback = onQuitCallback;
+        _onDoneCallback = onDoneCallback;
+    }
+
+    protected override void Enter()
+    {
+        foreach (BattleUnit battleUnit in _units)
+        {
+            battleUnit.Set(StatsManager.Defense, 0);
+            battleUnit.Set(StatsManager.Speed, 0);
+            battleUnit.TurnStarted();
+        }
+        
+        if (_turnType == TurnType.Enemy)
+            SwitchState(new EnemyTurnState(_units[0], (Enemy)_units[1],_onQuitCallback,  _onDoneCallback));
+        else
+            SwitchState(new PlayerTurnState(_units[0], (Enemy)_units[1], _deckBattleData, _onQuitCallback, _onDoneCallback));
+    }
+
+    protected override void Exit()
+    {
+        foreach (BattleUnit battleUnit in _units)
+        {
+            battleUnit.TurnEnd();
+        }
+
+    }
+}
